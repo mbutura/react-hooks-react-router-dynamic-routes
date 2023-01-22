14694bd6c5c0a7c9d6e768b136d723a776f46003
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-01-22 12:45:36 +0000

    Complete code along

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
 function App() {
+  // eslint-disable-next-line
   const [movies, setMovies] = useState({
     1: { id: 1, title: "A River Runs Through It" },
     2: { id: 2, title: "Se7en" },
@@ -13,14 +14,10 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Switch>
-        <Route path="/movies">
-          <MoviesPage movies={movies} />
-        </Route>
-        <Route exact path="/">
-          <div>Home</div>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/movies/*" element={<MoviesPage movies={movies} />} />
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
     </div>
   );
 }
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,19 @@
+// .src/components/MoviesPage.js
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Routes>
+        <Route path="/:movieId" element={<MovieShow movies={movies}/>}/>
+      </Routes>
     </div>
   );
 }
+
 export default MoviesPage;
